cd2b6d2d72dfc2c447ce77e2f389522a967a75bd
Dominic Lakshan <dominiclakshank@gmail.com>
2024-09-19 19:38:21 +0530

    initializing spring boot project

---
+++ src/main/java/com/gmart/gmart_api/GMartApiApplication.java
@@ -0,0 +1,13 @@
+package com.gmart.gmart_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GMartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GMartApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmart/gmart_api/GMartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmart.gmart_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GMartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
