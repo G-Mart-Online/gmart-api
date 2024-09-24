8cf5dca36a890d1cf812f1b0de052b933ea26e8e
Nimesh Nuwanga <ymnimesh@gmail.com>
2024-09-24 21:33:31 +0530

    add authentication config

---
+++ src/main/java/com/gmart/gmart_api/config/UserAuthenticationEntryPoint.java
@@ -0,0 +1,42 @@
+package com.gmart.gmart_api.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gmart.gmart_api.dto.ErrorDto;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class UserAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserAuthenticationEntryPoint.class);
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+        logger.warn("Unauthorized access attempt: {}", authException.getMessage());
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+
+        try {
+            objectMapper.writeValue(response.getOutputStream(), new ErrorDto("Unauthorized path"));
+        } catch (IOException e) {
+            logger.error("Error writing to the response output stream", e);
+            // Handle or log the exception appropriately
+            throw e;
+        }
+    }
+}
---
+++ src/main/java/com/gmart/gmart_api/config/UserAuthenticationProvider.java
@@ -0,0 +1,95 @@
+package com.gmart.gmart_api.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.gmart.gmart_api.dto.UserDto;
+import com.gmart.gmart_api.service.UserService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Base64;
+import java.util.Collections;
+import java.util.Date;
+
+@Component
+public class UserAuthenticationProvider {
+
+    private static final long TOKEN_VALIDITY_DURATION = 24 * 60 * 60 * 1000L; // 1 day in milliseconds
+    private static final String FIRST_NAME_CLAIM = "firstName";
+    private static final String LAST_NAME_CLAIM = "lastName";
+    private final UserService userService;
+    @Value("${security.jwt.token.secret-key:secret-key}")
+    private String secretKey;
+
+    @Autowired
+    public UserAuthenticationProvider(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostConstruct
+    protected void init() {
+        // This is to avoid having the raw secret key available in the JVM
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createToken(UserDto user) {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + TOKEN_VALIDITY_DURATION);
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        return JWT.create()
+                .withSubject(user.getUsername())
+                .withIssuedAt(now)
+                .withExpiresAt(validity)
+                .withClaim(FIRST_NAME_CLAIM, user.getFirstName())
+                .withClaim(LAST_NAME_CLAIM, user.getLastName())
+                .sign(algorithm);
+    }
+
+    public Authentication validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .build();
+
+            DecodedJWT decoded = verifier.verify(token);
+
+            UserDto user = new UserDto.Builder()
+                    .username(decoded.getSubject())
+                    .firstName(decoded.getClaim(FIRST_NAME_CLAIM).asString())
+                    .lastName(decoded.getClaim(LAST_NAME_CLAIM).asString())
+                    .build();
+
+            return new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
+        } catch (JWTVerificationException e) {
+            // For simplicity, rethrowing the exception here
+            throw e;
+        }
+    }
+
+    public Authentication validateTokenStrongly(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .build();
+
+            DecodedJWT decoded = verifier.verify(token);
+
+            UserDto user = userService.findByUsername(decoded.getSubject());
+
+            return new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
+        } catch (JWTVerificationException e) {
+            // For simplicity, rethrowing the exception here
+            throw e;
+        }
+    }
+}
