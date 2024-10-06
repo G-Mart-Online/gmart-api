38feaa982411096ffec4361e34dddace6f1e26f5
Nimesh Nuwanga <ymnimesh@gmail.com>
2024-10-06 11:35:37 +0530

    Update OrderController.java

--- src/main/java/com/gmart/gmart_api/controller/OrderController.java
+++ src/main/java/com/gmart/gmart_api/controller/OrderController.java
@@ -5,12 +5,13 @@ import com.gmart.gmart_api.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/order")
+@RequestMapping("/api/v1/orders")
 public class OrderController {
 
     private final OrderService orderService;
@@ -20,18 +21,21 @@ public class OrderController {
         this.orderService = orderService;
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_SELLER"})
     @PostMapping
     public ResponseEntity<Order> createOrder(@RequestBody Order order) {
         Order savedOrder = orderService.saveOrder(order);
         return new ResponseEntity<>(savedOrder, HttpStatus.CREATED);
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_SELLER"})
     @GetMapping("/list")
     public ResponseEntity<List<Order>> getAllOrders() {
         List<Order> orders = orderService.getAllOrders();
         return new ResponseEntity<>(orders, HttpStatus.OK);
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_SELLER"})
     @GetMapping("/{orderId}")
     public ResponseEntity<Order> getOrderById(@PathVariable String orderId) {
         Order order = orderService.getOrderById(orderId);
@@ -43,12 +47,14 @@ public class OrderController {
         }
     }
 
-    @DeleteMapping("/delete/{orderId}")
+    @Secured({"ROLE_ADMIN"})
+    @DeleteMapping("/{orderId}")
     public ResponseEntity<Void> deleteOrderById(@PathVariable String orderId) {
         orderService.deleteOrderById(orderId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_SELLER"})
     @PutMapping("/update-status/{orderId}")
     public ResponseEntity<Order> updateOrderStatus(@PathVariable String orderId, @RequestParam String status) {
         Order updatedOrder = orderService.updateOrderStatus(orderId, status);
@@ -58,5 +64,4 @@ public class OrderController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-
 }
